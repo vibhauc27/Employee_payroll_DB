4da20c7d2124b8fc56f676a986d74b0501d31895
Vibhauc_27 <ucvibha@gmail.com>
2022-03-24 07:58:02 +0530

    [Vibha]Added:To Create Employee payroll table

--- Employee_payroll_Database.sql
+++ Employee_payroll_Database.sql
@@ -1,6 +1,17 @@
+--UC-1
 create database payroll_service
 --show database 
-
 use payroll_service
 select DB_NAME()
+
+--UC-2
+create table employee_payroll
+(
+id int not null,
+name varchar(25) not null,
+salary money not null,
+start_date date not null
+);
+
+
 
