a615f4c7646e97a20f8e8ab7f54d20b5b5e70cc4
Vibhauc_27 <ucvibha@gmail.com>
2022-03-24 08:02:04 +0530

    [Vibha]Added:To Insert data into the table

--- Employee_payroll_Database.sql
+++ Employee_payroll_Database.sql
@@ -13,5 +13,9 @@ salary money not null,
 start_date date not null
 );
 
-
+--UC-3
+insert into employee_payroll values
+(101,'Billi',100000.00,'2018-01-03'),
+(102,'Terisa',200000.00,'2019-11-13'),
+(103,'Charlie',300000.00,'2020-05-21')
 
