11d77d0bb8b4a007d6fdb2bf770f72060f7c954f
Vibhauc_27 <ucvibha@gmail.com>
2022-03-29 17:37:11 +0530

    [Vibha]Added:Implementing the ER diagram and UC-12

--- Employee_payroll_Database.sql
+++ Employee_payroll_Database.sql
@@ -79,3 +79,123 @@ values('1','Terissa','2021-08-02','F','8687653523','Marketing',50000,2000,500,25
 select * from employee_payroll where name='Terissa';
 
 
+
+--Normalization
+
+create table employee_payroll_table
+(
+id int primary key identity(1,1),
+name varchar(100),
+startdate Date,
+gender varchar(10),
+phonenumber varchar(20),
+address varchar(200)
+);
+
+select * from employee_payroll_table;
+
+insert into employee_payroll_table(name,startdate,gender,phonenumber,address)
+values('John','2022-01-01','M','6876543456','MP'),
+('Rachel','2022-03-03','F','7890876556','Mumbai'),
+('Terissa','2022-02-01','F','8890987667','Karnataka'),
+('Bill','2021-05-01','M','7878654567','Pune'),
+('Charlie','2021-07-01','M','9987654545','Delhi');
+
+insert into employee_payroll_table(name,startdate,gender,phonenumber,address)
+values('Terissa','2021-05-01','F','8890987667','Karnataka');
+
+
+
+create table departmentdeatils(
+DeptId int primary key identity(1,1),
+DepartmentName Varchar(100),
+id int FOREIGN KEY REFERENCES employee_payroll_table(id)
+);
+
+select * from departmentdeatils;
+
+insert into departmentdeatils(DepartmentName,id)
+values('Marketing',1);
+
+insert into departmentdeatils(DepartmentName,id)
+values('HR',2);
+
+insert into departmentdeatils(DepartmentName,id)
+values('Sales',3);
+
+insert into departmentdeatils(DepartmentName,id)
+values('Quality Analysis',4);
+
+insert into departmentdeatils(DepartmentName,id)
+values('HR',5);
+
+insert into departmentdeatils(DepartmentName,id)
+values('Marketing',6);
+
+select name,startdate,gender,phonenumber,address,DepartmentName from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id;
+
+
+
+
+create table SalaryDetails(
+SalaryId int primary key identity(1,1),
+BasicPay decimal,
+Deductions decimal,
+TaxablePay decimal,
+IncomeTax decimal,
+NetPay decimal,
+id int FOREIGN KEY REFERENCES employee_payroll_table(id)
+);
+
+insert into SalaryDetails(BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,id)
+values(50000,2000,200,500,48000,1);
+
+insert into SalaryDetails(BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,id)
+values(50000,500,200,500,49500,2);
+
+insert into SalaryDetails(BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,id)
+values(80000,3000,200,500,77000,3);
+
+insert into SalaryDetails(BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,id)
+values(50000,1000,200,500,49000,4);
+
+insert into SalaryDetails(BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,id)
+values(60000,500,200,500,59500,5);
+
+insert into SalaryDetails(BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,id)
+values(55000,1000,200,500,54000,6);
+
+
+select employee_payroll_table.id,name,phonenumber,address,DepartmentName,gender,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,startdate from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id;
+
+select * from SalaryDetails;
+
+
+
+
+--UC11
+--Implement the ER Diagram into Payroll Service DB
+
+select sum(BasicPay) as Sum, gender from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id group by gender;
+
+select avg(BasicPay) as AveragePay, gender from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id group by gender;
+
+select max(BasicPay) as MaxPay, gender from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id group by gender;
+
+select min(BasicPay) as MinPay, gender from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id group by gender;
+
+select count(BasicPay) as Count, gender from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id group by gender;
+
+
+
+--UC12 
+--Ensure all retrieve queries done especially in UC 4, UC 5 and UC 7 are working with new table structure
+
+--UC4
+select employee_payroll_table.id,name,phonenumber,address,DepartmentName,gender,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,startdate from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id;
+
+--UC5
+select BasicPay,name from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id where name='Bill';
+
+select employee_payroll_table.id,name,phonenumber,address,DepartmentName,gender,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,startdate from employee_payroll_table inner join departmentdeatils on employee_payroll_table.id=departmentdeatils.id inner join SalaryDetails on employee_payroll_table.id=SalaryDetails.id where startdate between cast('2022-02-01' as date) AND cast(SYSDATETIME() as date);
+
